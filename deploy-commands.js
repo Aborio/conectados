a56c974045c3de13237ca91bb10db0011c4a72c2
Agustin Borio <96600815+Aborio@users.noreply.github.com>
2025-10-27 19:36:16 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,7 +25,7 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-(async () => {
+async function main() {
   try {
     if (process.env.GUILD_ID) {
       await rest.put(
@@ -34,11 +34,14 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
       );
       console.log('✅ Comandos (guild) registrados.');
     } else {
-      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+      await rest.put(
+        Routes.applicationCommands(process.env.CLIENT_ID),
+        { body: commands }
+      );
       console.log('✅ Comandos (globales) registrados.');
     }
   } catch (err) {
     console.error('❌ Error registrando comandos:', err);
   }
-})();
-
+}
+main();
